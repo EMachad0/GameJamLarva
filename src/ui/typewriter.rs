3448cd39ee425306ef96b7e7b111e4096b543667
EMachad0 <elitonmachadod200@gmail.com>
2022-10-15 20:49:31 -0300

    typewriter skip

--- src/ui/typewriter.rs
+++ src/ui/typewriter.rs
@@ -1,29 +1,26 @@
 use bevy::prelude::*;
+use bevy::time::Stopwatch;
+use std::time::Duration;
 
 #[derive(Debug, Default, Component)]
 pub struct Typewriter {
     finished: bool,
+    time_since_finished: Stopwatch,
 }
 
 #[derive(Debug, Deref, DerefMut)]
 pub struct TypingTimer(pub Timer);
 
-#[derive(Debug, Deref, DerefMut)]
-pub struct TypewriterFinished {
-    entity: Entity,
-}
-
 pub fn typewriter_update(
     time: Res<Time>,
     mut timer: ResMut<TypingTimer>,
-    mut query: Query<(Entity, &mut Text, &mut Typewriter)>,
-    mut finished_evw: EventWriter<TypewriterFinished>,
+    mut query: Query<(&mut Text, &mut Typewriter)>,
 ) {
     if !timer.tick(time.delta()).just_finished() {
         return;
     }
 
-    for (entity, mut text, mut typewriter) in query.iter_mut() {
+    for (mut text, mut typewriter) in query.iter_mut() {
         while !text.sections[1].value.is_empty() {
             let c = text.sections[1].value.remove(0);
             text.sections[0].value.push(c);
@@ -34,7 +31,29 @@ pub fn typewriter_update(
 
         if text.sections[1].value.is_empty() {
             typewriter.finished = true;
-            finished_evw.send(TypewriterFinished { entity });
+        }
+    }
+}
+
+pub fn typewriter_skip_input(
+    kbd: Res<Input<KeyCode>>,
+    mut query: Query<&mut Text, With<Typewriter>>,
+) {
+    if kbd.pressed(KeyCode::Escape) {
+        for mut text in &mut query {
+            if text.sections[1].value.is_empty() {
+                continue;
+            }
+            let remaining = std::mem::take(&mut text.sections[1].value);
+            text.sections[0].value += &remaining;
+        }
+    }
+}
+
+pub fn finished_typewriter_update(time: Res<Time>, mut query: Query<&mut Typewriter>) {
+    for mut typewriter in query.iter_mut() {
+        if typewriter.finished {
+            typewriter.time_since_finished.tick(time.delta());
         }
     }
 }
@@ -55,7 +74,7 @@ pub fn to_typewriter_sections(original: TextSection) -> [TextSection; 2] {
     ]
 }
 
-pub fn on_typewriter_finish<C: Component>(query: Query<&Typewriter, With<C>>) -> bool {
+pub fn after_typewriter_finish<C: Component>(query: Query<&Typewriter, With<C>>) -> bool {
     let typewriter = query.get_single().expect("entity without typewriter");
-    typewriter.finished
+    typewriter.time_since_finished.elapsed() >= Duration::from_secs(1)
 }
