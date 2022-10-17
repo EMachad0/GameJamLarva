f1faf97404452f0801971f9380629208a5cccb52
EMachad0 <elitonmachadod200@gmail.com>
2022-10-17 03:10:58 -0300

    windows icon

--- build.rs
+++ build.rs
@@ -3,6 +3,6 @@ extern crate embed_resource;
 fn main() {
     let target = std::env::var("TARGET").unwrap();
     if target.contains("windows") {
-        embed_resource::compile("icon.rc");
+        embed_resource::compile("build/windows/icon.rc");
     }
 }
